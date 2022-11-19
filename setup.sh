1b873ee4c3e85758ddf6b6a40db877c957246826
giu-ferreira-cientista <giuferreira@gmail.com>
2022-11-19 03:53:57 +0000

    Config Updated

--- setup.sh
+++ setup.sh
@@ -28,26 +28,6 @@ up() {
 }
 
 config() {
-  echo "Connecting Airflow with Airbyte..."
-  echo "Enter your Airbyte Epidemiology connection ID: "
-  read epidemiology_connection_id
-
-  echo "Enter your Airbyte Economy connection ID: "
-  read economy_connection_id
-
-  echo "Enter your Airbyte Demographics connection ID: "
-  read demographics_connection_id
-
-  echo "Enter your Airbyte Index connection ID: "
-  read index_connection_id
-
-  # Set connection IDs for DAG.
-  cd airflow
-  docker-compose run airflow-webserver airflow variables set 'AIRBYTE_EPIDEMIOLOGY_CONNECTION_ID' "$epidemiology_connection_id"
-  docker-compose run airflow-webserver airflow variables set 'AIRBYTE_ECONOMY_CONNECTION_ID' "$economy_connection_id"
-  docker-compose run airflow-webserver airflow variables set 'AIRBYTE_DEMOGRAPHICS_CONNECTION_ID' "$demographics_connection_id"
-  docker-compose run airflow-webserver airflow variables set 'AIRBYTE_INDEX_CONNECTION_ID' "$index_connection_id"
-
   docker network create modern-data-stack
   docker network connect modern-data-stack airbyte-proxy
   docker network connect modern-data-stack airbyte-worker  
@@ -55,8 +35,10 @@ config() {
   docker network connect modern-data-stack airflow-airflow-webserver-1
   docker network connect modern-data-stack metabase
   
+  cd airflow
   docker-compose run airflow-webserver airflow connections add 'airbyte_example' --conn-uri 'airbyte://airbyte-proxy:8000'
   cd ..
+  
   echo "Config Updated..."
 }
 
